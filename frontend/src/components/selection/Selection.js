9529f72d4c16d8a2cfe7d2355402b8aa254ff540
Wen Qi <wtoh008@e.ntu.edu.sg>
2020-09-25 00:57:57 +0800

    #5: added increment/decrement count buttons

--- frontend/src/components/selection/Selection.js
+++ frontend/src/components/selection/Selection.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import SelectionItem from "./SelectionItem";
 
 export default function Selection(data) {
     return (
         <div className="row">
             { data.data.map((bread) => {
                 return (
-                    <div className="col s4">
-                        {bread.name} ${bread.price/100}<br/>
-                        <img src={`http://localhost:4242/public/Images/${bread.imgName}`} width="200" height="200"/>
-                    </div>
+                    <SelectionItem key={bread.id} bread={bread} />
                 )
             }) }
         </div>
---
+++ frontend/src/components/selection/SelectionItem.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export default function SelectionItem(data) {
+    const [count, setCount] = useState(0);
+
+    function decrementCount() {
+        setCount((prevCount) => {
+            return (prevCount === 0) ? 0 : (prevCount - 1)
+        })
+    }
+
+    function incrementCount() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    const bread = data.bread;
+    return (
+        <div className="col s4">
+            <div className="center">
+                <h5>{bread.name}</h5>
+                <img src={`http://localhost:4242/public/Images/${bread.imgName}`} width="200" height="200"/>
+                <p className="light">${bread.price/100}</p>
+                <button className="btn-floating waves-effect waves-light btn-small" style={{"marginRight":"10px"}} onClick={decrementCount}>-</button>
+                <span>{count}</span>
+                <button className="btn-floating waves-effect waves-light btn-small" style={{"marginLeft":"10px"}} onClick={incrementCount}>+</button>
+            </div>
+        </div>
+        
+    )
+}
